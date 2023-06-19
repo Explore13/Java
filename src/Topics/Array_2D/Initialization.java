7a6009291e244c2355143dcd0013a34ab072e40c
Explore13 <suryaghoshchemistry2k21@gmail.com>
2023-06-19 17:07:24 +0530

    Array's File Added

---
+++ src/Topics/Array_2D/Initialization.java
@@ -0,0 +1,17 @@
+package Topics.Array_2D;
+
+import java.sql.SQLOutput;
+
+public class Initialization {
+    // Welcome to Explorer's JDK
+        public static void main(String[] args) {
+    int[][] nums =new int[3][2];
+    for (int i=0;i<3;i++){
+        for (int j=0;j<2;j++){
+            System.out.print(nums[i][j]+" ");
+        }
+        System.out.println();
+    }
+        }
+
+}
