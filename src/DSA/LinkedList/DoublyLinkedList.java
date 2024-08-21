056f55c199080e4db3b6469413bb6d1554711f54
Explore13 <suryaghoshchemistry2k21@gmail.com>
2024-08-21 07:51:33 +0530

    Done : DoublyLinkedList completed

--- src/DSA/LinkedList/DoublyLinkedList.java
+++ src/DSA/LinkedList/DoublyLinkedList.java
@@ -1,5 +1,7 @@
 package DSA.LinkedList;
 
+// Doubly linked list is used in "Browser"
+
 class DoublyNode {
     public int data;
     public DoublyNode prev;
@@ -38,7 +40,7 @@ class DoublyNode {
 
 public class DoublyLinkedList {
 
-    // create LL from Array
+    // Create LL from Array
     private static DoublyNode createLL(int[] arr) {
         DoublyNode head = new DoublyNode(arr[0]);
         DoublyNode prevNode = head;
@@ -166,6 +168,67 @@ public class DoublyLinkedList {
         return head;
     }
 
+    // Insert before head
+    private static DoublyNode insertBeforeHead(DoublyNode head, int value) {
+        DoublyNode newNode = new DoublyNode(value);
+        if (head == null) return newNode;
+        head.prev = newNode;
+        newNode.next = head;
+        head = newNode;
+        return head;
+    }
+
+    // Insert before tail
+    private static DoublyNode insertBeforeTail(DoublyNode head, int value) {
+        if (head == null) return null;
+        if (head.next == null) return insertBeforeHead(head, value);
+        DoublyNode temp = head;
+        while (temp.next != null)
+            temp = temp.next;
+        DoublyNode prevNode = temp.prev;
+        DoublyNode newNode = new DoublyNode(value, prevNode, temp);
+        prevNode.next = newNode;
+        temp.prev = newNode;
+        return head;
+    }
+
+    // Insert before Kth element
+    private static DoublyNode insertBeforeKthNode(DoublyNode head, int k, int value) {
+        if (head == null)
+            return null;
+
+        if (k == 1) return insertBeforeHead(head, value);
+        int cnt = 0;
+        DoublyNode temp = head;
+        while (temp != null) {
+            cnt++;
+            if (cnt == k) break;
+            temp = temp.next;
+        }
+        if (temp != null) {
+            DoublyNode prevNode = temp.prev;
+            DoublyNode newNode = new DoublyNode(value, prevNode, temp);
+            prevNode.next = newNode;
+            temp.prev = newNode;
+        }
+        return head;
+    }
+
+    // Insert before a node ( do not insert before head )
+    private static void insertBeforeNode(DoublyNode head, int value, int data)
+    {
+        DoublyNode temp = head;
+        while (temp != null) {
+            if (temp.data == data) break;
+            temp = temp.next;
+        }
+        if (temp != null && temp != head) {
+            DoublyNode prevNode = temp.prev;
+            DoublyNode newNode = new DoublyNode(value, prevNode, temp);
+            prevNode.next = newNode;
+            temp.prev = newNode;
+        }
+    }
     public static void main(String[] args) {
         int[] arr = {10, 20, 40, 50, 60, 70};
         DoublyNode head = createLL(arr);
@@ -184,9 +247,31 @@ public class DoublyLinkedList {
         head = deleteFromPosition(head, k);
         traverseLL(head);
 
-        // delete  particular node
+        // delete particular node
         int value = 20;
         head = deleteParticularNode(head, value);
+        traverseLL(head);
+
+        // insert before head
+        value = 100;
+        head = insertBeforeHead(head, value);
+        traverseLL(head);
+
+        // insert before tail
+        value = 500;
+        head = insertBeforeTail(head, value);
+        traverseLL(head);
+
+        // insert before Kth element
+        value = 200;
+        k = 3;
+        head = insertBeforeKthNode(head, k, value);
+        traverseLL(head);
+
+        // insert before a node
+        value = 400;
+        int data = 200;
+        insertBeforeNode(head,value,data);
         traverseLL(head);
     }
 }
