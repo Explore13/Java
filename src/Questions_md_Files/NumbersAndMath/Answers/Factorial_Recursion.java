6fe1e6b293ddc938632854712ff81bab23a29970
Explore13 <suryaghoshchemistry2k21@gmail.com>
2025-02-22 03:07:00 +0530

    factorail using recursion

---
+++ src/Questions_md_Files/NumbersAndMath/Answers/Factorial_Recursion.java
@@ -0,0 +1,23 @@
+package Questions_md_Files.NumbersAndMath.Answers;
+
+import java.util.Scanner;
+
+public class Factorial_Recursion {
+    static long fact (int n)
+    {
+        if(n == 0) return 1;
+        return n * fact(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int num = scan.nextInt();
+
+        if (num < 0) System.out.println("Invalid Number");
+        else {
+            long fact = fact(num);
+            System.out.println(num +"! = " + fact);
+
+        }
+    }
+}
