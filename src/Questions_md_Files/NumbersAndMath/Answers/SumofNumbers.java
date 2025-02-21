45e546f0b3d9e7335e1ed9864c4b85d3f2983357
Explore13 <suryaghoshchemistry2k21@gmail.com>
2025-02-22 00:43:09 +0530

    sum of n numbers added

--- src/Questions_md_Files/NumbersAndMath/Answers/SumofNumbers.java
+++ src/Questions_md_Files/NumbersAndMath/Answers/SumofNumbers.java
@@ -1,2 +1,18 @@
-package Questions_md_Files.NumbersAndMath.Answers;public class SumofNumbers {
+package Questions_md_Files.NumbersAndMath.Answers;
+
+import java.util.Scanner;
+
+public class SumofNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a range : ");
+        int num = scan.nextInt();
+
+        if (num < 0)
+            System.out.println(num + " must be a positive number.");
+        else {
+            float sum = (float) (num * (num +1) / 2.0);
+            System.out.println("Sum of the first " + num + " positive numbers : " + sum);
+        }
+    }
 }
