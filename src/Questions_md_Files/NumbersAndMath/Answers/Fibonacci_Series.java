4244ba996a1cc4dc47c28bc57ee1cb7fbd67be76
Explore13 <suryaghoshchemistry2k21@gmail.com>
2025-02-22 02:59:24 +0530

    fibonacci series

---
+++ src/Questions_md_Files/NumbersAndMath/Answers/Fibonacci_Series.java
@@ -0,0 +1,27 @@
+package Questions_md_Files.NumbersAndMath.Answers;
+
+import java.util.Scanner;
+
+public class Fibonacci_Series {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a range : ");
+        int range = scan.nextInt();
+
+        if (range < 0) System.out.println("Invalid Number");
+        else {
+            if(range == 0) System.out.println(0);
+            else if (range == 1) System.out.println(1);
+            else {
+                System.out.print("0, 1");
+                int last = 1, prev = 0, current = 0;
+                for(int i = 2; i <= range ; i++) {
+                    current = prev + last;
+                    prev = last;
+                    last = current;
+                    System.out.print(", " + current);
+                }
+            }
+        }
+    }
+}
