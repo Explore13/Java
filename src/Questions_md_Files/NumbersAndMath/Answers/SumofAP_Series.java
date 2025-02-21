a4f6b4f173ec11dbaa181590a89f783add5fcad0
Explore13 <suryaghoshchemistry2k21@gmail.com>
2025-02-22 01:01:32 +0530

    Sum of AP Series added

---
+++ src/Questions_md_Files/NumbersAndMath/Answers/SumofAP_Series.java
@@ -0,0 +1,21 @@
+package Questions_md_Files.NumbersAndMath.Answers;
+
+import java.util.Scanner;
+
+public class SumofAP_Series {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the first term : ");
+        float a = scan.nextFloat();
+        System.out.println("Enter the common difference : ");
+        float d = scan.nextFloat();
+        System.out.println("Enter the number of terms in A.P : ");
+        int n = scan.nextInt();
+
+        if (n <= 0) System.out.println("Invalid input");
+        else {
+            float sum = (float) (n/2) * ( (2 * a) + ((n-1)*d));
+            System.out.println("Sum of the series : " + sum);
+        }
+    }
+}
