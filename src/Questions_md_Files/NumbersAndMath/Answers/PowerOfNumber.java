dd41c2d6af3b799a294ec0ca830fb07d551a370f
Explore13 <suryaghoshchemistry2k21@gmail.com>
2025-02-22 03:09:51 +0530

    power of a number

---
+++ src/Questions_md_Files/NumbersAndMath/Answers/PowerOfNumber.java
@@ -0,0 +1,16 @@
+package Questions_md_Files.NumbersAndMath.Answers;
+
+import java.util.Scanner;
+
+public class PowerOfNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int num = scan.nextInt();
+        System.out.println("Enter the power : ");
+        int power = scan.nextInt();
+
+        double totalPower = Math.pow(num,power);
+        System.out.println(num + " to the power of " + power + " = " + totalPower);
+    }
+}
