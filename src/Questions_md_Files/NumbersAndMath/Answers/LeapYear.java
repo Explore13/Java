02f93b08327aa91ab08671d7fbb6f89f0bcee1f8
Explore13 <suryaghoshchemistry2k21@gmail.com>
2025-02-22 01:40:36 +0530

    leap year code added

---
+++ src/Questions_md_Files/NumbersAndMath/Answers/LeapYear.java
@@ -0,0 +1,20 @@
+package Questions_md_Files.NumbersAndMath.Answers;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        // divisible by 4 or divisible by 100 and divisible by 400
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a year : ");
+        int year = scan.nextInt();
+
+        if (year <= 0) System.out.println("Invalid input");
+        else {
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+                System.out.println(year + " is a leap year.");
+            else
+                System.out.println(year + " is not a leap year.");
+        }
+    }
+}
