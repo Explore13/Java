21352749efdbef68f9379547aa454530512160e7
Explore13 <suryaghoshchemistry2k21@gmail.com>
2026-05-12 22:25:33 +0530

    Stack Implementations with Prac Ques Added

--- src/Topics/_25_Stack/_1_Stack_Implementation.java
+++ src/Topics/_25_Stack/_1_Stack_Implementation.java
@@ -1,2 +1,50 @@
-package Topics.Stack;public class Stack_Implementation {
+package Topics._25_Stack;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+public class _1_Stack_Implementation {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<Integer>();
+
+        // push : to add an element into a stack
+        stack.push(50);
+        stack.push(40);
+        stack.push(30);
+        stack.push(20);
+        stack.push(10);
+        System.out.println("The Stack : " + stack);
+
+        // pop : remove the top most element from the stack
+        int item = stack.pop();
+        System.out.println("Item removed : " + item);
+        System.out.println("The Stack after removing the top most element : " + stack);
+
+        // peek : returns the top most element of the stack
+        System.out.println("The peek element is before pop : " + stack.peek());
+        stack.pop();
+        System.out.println("The peek element is after pop : " + stack.peek());
+
+        // search : search an element in a stack (1 based indexing from top)
+        System.out.println("The Stack : " + stack);
+        System.out.println("The position of 50 is : " + stack.search(50));
+        System.out.println("The position of 60 is : " + stack.search(60));
+
+        // empty : check if stack is empty or not (boolean)
+        System.out.println("Is Stack Empty : " + stack.empty());
+
+        // stack underflow condition
+        try {
+            int i = stack.pop();
+            System.out.println(i + " is popped out");
+            i = stack.pop();
+            System.out.println(i + " is popped out");
+            i = stack.pop();
+            System.out.println(i + " is popped out");
+            stack.pop();
+        } catch (EmptyStackException e) {
+            System.out.println("The Exception : " + e);
+        }
+
+    }
 }
--- src/Topics/_25_Stack/_2_Valid_Parentheses.java
+++ src/Topics/_25_Stack/_2_Valid_Parentheses.java
@@ -1,2 +1,35 @@
-package Topics._25_Stack;public class _2_Valid_Parentheses {
+package Topics._25_Stack;
+
+import java.util.*;
+
+public class _2_Valid_Parentheses {
+
+    private static boolean isValid(String str) {
+        if (str == null || str.isEmpty()) return false;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+                continue;
+            } else if (stack.isEmpty()) return false;
+
+            char peekChar = stack.peek();
+            if (ch == ')' && (peekChar == '{' || peekChar == '[')) return false;
+            if (ch == '}' && (peekChar == '(' || peekChar == '[')) return false;
+            if (ch == ']' && (peekChar == '(' || peekChar == '{')) return false;
+            stack.pop();
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String str = "{[]()}";
+        System.out.println(isValid(str));
+    }
 }
+
+/*
+Time Complexity : O(n)
+Space Complexity : O(n)
+*/
